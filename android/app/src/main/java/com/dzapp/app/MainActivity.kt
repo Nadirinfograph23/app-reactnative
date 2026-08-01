0a5df0230242c38d55ea402d34d67df0aebdc00b
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-08-01 02:34:03 +0100

    ⚛️ DZ Agent: add android/app/src/main/java/com/dzapp/app/MainActivity.kt

---
+++ android/app/src/main/java/com/dzapp/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dzapp.app
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+  override fun getMainComponentName(): String = "app"
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
